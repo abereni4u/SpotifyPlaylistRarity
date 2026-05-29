2dffa9337afc0b82c972b9289aee7cf69cd31ef5
Abereni4u <abereni4u@gmail.com>
2026-05-28 23:16:55 -0400

    Remove Chosic token extraction code to pursue manual CSV input method

--- src/main/java/org/example/PlaylistExtractor.java
+++ src/main/java/org/example/PlaylistExtractor.java
@@ -1,52 +1,37 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 import java.io.IOException;
-import java.net.CookieManager;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+
+
 
 public class PlaylistExtractor {
 
-    private static final String CHOSIC_PAGE = "https://www.chosic.com/spotify-playlist-exporter/";
-    private static final String CHOSIC_TOKEN_URL = "https://www.chosic.com/api/tools/t/";
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        getChosicToken();
+    public static void main(String[] args)  {
+        // User will get playlist CSV from chosic.com
+        // Launch broswer with page, start listener / watchService on downloads folder
+        // User places spotify link and downloads CSV
+        // program watches download folder for CSV and parsing begins
+        // Parse CSV here
+        // Use Reccobeats and track IDs to create track objects for each playlist item
+            // Playlist Object
+                // Array list of Track Objects
+            // Track Objects
+                // Song Title
+                // Artist
+                // BPM
+                // Camelot
+                // Energy
+                // Duration
+                // Popularity
+                // Genres
+                // Album
+                // Features ~ Dance, Acoustic, Instrumental, Valence, Speech, Live, Loud, Key, Signature
+                // Track ID
+        // --- containing audio features, and the title
+        // Public method for converting the playlist into a group of objects with features
     }
 
-    public static void getChosicToken() throws IOException, InterruptedException {
-        HttpClient cookieGrabber = HttpClient.newBuilder()
-                .cookieHandler(new CookieManager()).build();
 
-        HttpRequest cookieRequest = HttpRequest.newBuilder()
-                .uri(URI.create(CHOSIC_PAGE))
-                .header("User-Agent", "Mozilla/5.0")
-                .GET()
-                .build();
-
-        HttpResponse<String> cookieResponse = cookieGrabber.send(cookieRequest, HttpResponse.BodyHandlers.ofString());
-
-
-        String postBody = "app=playlist_analyzer";
-
-        cookieRequest = HttpRequest.newBuilder()
-                .uri(URI.create(CHOSIC_TOKEN_URL))
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .header("Origin", "https://www.chosic.com")
-                .header("Referer", CHOSIC_PAGE)
-                .header("X-Requested-With", "XMLHttpRequest")
-                .POST(HttpRequest.BodyPublishers.ofString(postBody))
-                .build();
-
-        cookieResponse = cookieGrabber.send(cookieRequest, HttpResponse.BodyHandlers.ofString());
-
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode jNode = mapper.readTree(cookieResponse.body());
-        System.out.println(jNode.get("token").asText());
-    }
 }
